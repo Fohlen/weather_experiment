8b49a4a78dfbfa96b409df070a782e4361a1f9f4
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2024-05-11 16:22:10 +0200

    Rewrite script without SQLite

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import argparse
 import datetime
-import sqlite3
-from pathlib import Path
+import json
+from collections import namedtuple
+import sys
+
 from wetterdienst.provider.dwd.mosmix import DwdMosmixRequest, DwdMosmixType
 
-PARAMETERS = {
-    'temperature_air_mean_200': 0,
-    'precipitation_height_significant_weather_last_1h': 1
-}
+_KEYS = ["recorded_at", "station_id", "forecast_time", "parameter", "value"]
+Forecast = namedtuple("Forecast", _KEYS)
 
 
-def log_forecasts(database_file: Path, station_ids: list[str]):
-    db = sqlite3.Connection(database_file)
-
-    cursor = db.cursor()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS forecasts (
-        recorded_at INTEGER NOT NULL,
-        station_id INTEGER NOT NULL,
-        forecast_time INTEGER NOT NULL,
-        forecast_type INTEGER NOT NULL,
-        value REAL NOT NULL
-    );
-    """)
-
-    db.commit()
-
+def retrieve_forecasts(station_ids: list[str]):
     request = DwdMosmixRequest(
         parameter=["ttt", "rr1c"],
         mosmix_type=DwdMosmixType.SMALL,
@@ -35,22 +20,22 @@ def log_forecasts(database_file: Path, station_ids: list[str]):
     recorded_at = datetime.datetime.now()
 
     for result in stations.values.query():
-        rows = [
-            (recorded_at, station_id, date, PARAMETERS[parameter], value)
+        yield from [
+            (recorded_at, station_id, date, parameter, value)
             for (station_id, dataset, parameter, date, value, quality) in result.df.rows()
         ]
-        cursor.executemany("""
-        INSERT INTO forecasts(recorded_at, station_id, forecast_time, forecast_type, value) 
-        VALUES (?, ?, ?, ?, ?)
-        """, rows)
-
-        db.commit()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("database_file", type=Path)
     parser.add_argument("station_id", nargs="+", type=str)
     args = parser.parse_args()
 
-    log_forecasts(args.database_file, args.station_id)
+    print(f"Fetching forecasts for {len(args.station_id)} stations", file=sys.stderr)
+    forecasts = retrieve_forecasts(args.station_id)
+
+    for row in forecasts:
+        print(
+            json.dumps(dict(zip(_KEYS, row)), sort_keys=True, default=str),
+            file=sys.stdout
+        )
